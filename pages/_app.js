4916416016566ad9079c0721b2edb69cad48378d
jdawsonimgix <jdawson@imgix.com>
2022-06-22 11:43:11 -0700

    added TailwindUI header for imgix searching

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Flyoutmenu from './Flyoutmenu'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Flyoutmenu />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
